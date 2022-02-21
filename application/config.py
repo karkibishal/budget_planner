178f3474781b5e31c67e9de39598fa25a30db459
karkibishal <63976630+karkibishal@users.noreply.github.com>
2022-02-21 16:16:54 +0000

    Update config.py

--- application/config.py
+++ application/config.py
@@ -3,6 +3,6 @@ import os
 FLASK_ENV = 'development'
 DEBUG = True
 TESTING = True
-SECRET_KEY = os.getenv('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
